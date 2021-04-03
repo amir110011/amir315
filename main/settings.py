f304bc7e962d9ce15dc3f83b0338973781c8bfef
amir <mohamadamirnabavi@gmail.com>
2021-04-03 09:38:02 +0430

    fixed

--- main/settings.py
+++ main/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+LOGIN_REDIRECT_URL='account:home'
+LOGIN_URL='account:login'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -39,9 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'account.apps.AccountConfig',
     'extentions',
     'ckeditor',
     'ckeditor_uploader',
+    'widget_tweaks',
 ]
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
@@ -60,7 +63,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- main/urls.py
+++ main/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckrditor/', include('ckeditor_uploader.urls')),
     path('', include('blog.urls')),
+    path('account/', include('account.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
